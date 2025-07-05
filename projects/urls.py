0ae31b0a3985c4499bd41955886e572d2240ba52
tae <moon@git.com>
2025-07-05 19:32:58 +0900

    중간점검 2

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,5 @@ app_name = 'projects'
 urlpatterns = [
     path('', views.project_list, name='project_list'),
     path('<int:pk>/', views.project_detail, name='project_detail'),
-
+    path('<int:pk>/vote/', views.project_vote, name='project_vote'),
 ]
